99bf85f856f58810f25d15b17a8ee5d930befdfa
xiaohoucc <1373916199@qq.com>
2022-04-10 14:48:24 +0800

    login画面追加

---
+++ src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <el-form :model="loginForm" class="loginContainer">
+      <h3 class="loginTitle">系统登录</h3>
+      <el-form-item>
+        <el-input type="text" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" v-model="loginForm.password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="text" v-model="loginForm.code" placeholder="点击图片更换验证码" style="width: 250px;margin-right: 5px"></el-input>
+        <img :src="captchaUrl">
+      </el-form-item>
+      <el-checkbox v-model="checked" class="loginRemember">记住我</el-checkbox>
+      <el-button type="primary" style="width: 100%">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      captchaUrl: '',
+      checked: true,
+      loginForm: {
+        username: 'admin',
+        password: '123456',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.loginContainer {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 188px auto;
+  width: 350px;
+  padding: 15px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
+
+.loginTitle {
+  margin: 8px auto 40px auto;
+  text-align: center;
+}
+
+.loginRemember {
+  text-align: left;
+  margin: 8px 0px 15px 8px;
+}
+</style>
